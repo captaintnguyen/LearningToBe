1c4daca2eec69348844c120da7975c73017c28fc
captaintnguyen <52840151+captaintnguyen@users.noreply.github.com>
2019-07-12 15:36:11 -0700

    Update diceroll.py

--- diceroll.py
+++ diceroll.py
@@ -2,20 +2,23 @@
 
 import random
 
+#Initialize variables
 min = 1
 max = 6
 i = 0
 j = 0
 k = []
+
+#Define function for average
 def average(lst):
     return sum(lst)/len(lst)
 
-
+#User inputs
 roll = input("Would you like to roll dice? (Y/N)")
-
 how_many = input("How many rolls would you like to do?")
 count = int(how_many)
 
+#Actual loop for dice roll based on user input w/ average too!
 while roll == "Y" or roll == "y":
     print ("Rolling dies...")
     print ("The dice values are...")
